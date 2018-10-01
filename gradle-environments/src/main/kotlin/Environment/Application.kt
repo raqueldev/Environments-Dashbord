fe3d9805ec5701de2d46a7cd753b52029cdcebe2
raquelita <rvmederos@gmail.com>
2018-10-01 22:28:15 +0200

    Create DB connection and repositories

--- gradle-environments/src/main/kotlin/Environment/Application.kt
+++ gradle-environments/src/main/kotlin/Environment/Application.kt
@@ -4,6 +4,8 @@ import io.ktor.routing.routing
 import Environment.dao.*
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
 import io.ktor.routing.Routing
 
 
@@ -11,7 +13,14 @@ fun Application.main () {
 
     val dbStorage = EnvironmentDatabase() //TODO set database
 
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+            create()
+        }
+    }
     install(Routing)
+
 
     routing {
         environments(dbStorage)
